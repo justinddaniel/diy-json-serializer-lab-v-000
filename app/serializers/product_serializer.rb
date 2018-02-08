453e4d3f87226b7129f4d7cda68935a6b8977e76
Justin Daniel <justinddaniel@gmail.com>
2018-02-08 23:20:11 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,14 +5,10 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"title": "' + product.title + '", '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '"inventory": "' + product.inventory + '", '
     serialized_product += '"description": "' + product.description + '", '
-
-    # the author association can also be represented in JSON
-    serialized_product += '"author": {'
-    serialized_product += '"name": "' + product.author.name + '"}'
-
-    # and end with the close brace
     serialized_product += '}'
   end
 end
